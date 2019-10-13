405c301e3b69dc349925454abd3ea384f93924a5
atomspac <atomand.space@gmail.com>
2019-10-13 20:24:50 +0530

    added hello_gui.py

---
+++ hello_gui.py
@@ -0,0 +1,43 @@
+# import everything from Tkinter
+from Tkinter import *
+
+# create a class "Application"
+class Application(Frame):
+    # response when say_hi function is called upon
+    def say_hi(self):
+        print "Hello world!"
+
+    # create Widegets
+    def createWidgets(self):
+        # create QUIT Button
+        self.QUIT = Button(self)
+        # Text on the Button
+        self.QUIT["text"] = "QUIT"
+        # foreground color
+        self.QUIT["fg"]   = "red"
+        # background color
+        self.QUIT["bg"]   = "blue"
+        # command associated with the click
+        self.QUIT["command"] =  self.quit
+        # position of the button
+        self.QUIT.pack({"side": "right"})
+
+        # second Button
+        self.hi_there = Button(self)
+        self.hi_there["text"] = "Hello",
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack({"side": "right"})
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+# initialize Tkinter
+root = Tk()
+# giving the main window a name
+root.title("Hello")
+
+app = Application(master=root)
+app.mainloop()
+root.destroy()
--- pgui.py
+++ pgui.py
@@ -1,10 +1,12 @@
 import Tkinter as tk
 
 # create main main_window m
-m=tk.Tk(screenName=None, baseName=None, className='Tk', useTk=1)
+m=tk.Tk()
 # rename the main_window
 m.title('Hello there!')
-
+# add a button, b
+button=tk.Button(m, text='Stop', width=25, command=m.dest)
+button.pack()
 '''
 Widegets Space
 '''
